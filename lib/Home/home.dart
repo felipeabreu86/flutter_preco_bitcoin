523eb23c2ae54a7a40ad780ba63241f7aead202e
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-16 15:05:43 -0300

    Se aplicado, este commit irá criar a lógica de exibição do preço do bitcoin ao se clicar no botão Atualizar

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +10,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _preco = "0";
+  String _url = "https://blockchain.info/ticker";
+
+  void _recuperarPreco() async {
+    http.Response response = await http.get(Uri.parse(_url));
+    Map<String, dynamic> retorno = json.decode(response.body);
+    setState(() {
+      _preco = retorno["BRL"]?["buy"]?.toString() ?? "0";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/bitcoin.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Text(
+                  "R\$ " + _preco,
+                  style: TextStyle(fontSize: 35),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _recuperarPreco,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.orange),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 8, bottom: 8, left: 20, right: 20),
+                  child: Text(
+                    "Atualizar",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
